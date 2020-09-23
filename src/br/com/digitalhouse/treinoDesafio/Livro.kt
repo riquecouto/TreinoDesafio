ce8df35dcefd92bc18813976ae7c5ce6d2d7b32f
LeviJunior12 <levicoc123@gmail.com>
2020-09-23 17:03:28 -0300

    arrumando livro

--- src/br/com/digitalhouse/treinoDesafio/Livro.kt
+++ src/br/com/digitalhouse/treinoDesafio/Livro.kt
@@ -1,5 +1,5 @@
 package br.com.digitalhouse.treinoDesafio
 
-class Livro (val titulo: String, val autor: String, val anoDeLancamento: Int): Item{
+class Livro(val titulo: String, val autor: String, val anoDeLancamento: Int, override var codigo: Int, override var preco: Double, override var quantidadeEmEstoque: Int) : Item {
 
 }
--- src/br/com/digitalhouse/treinoDesafio/Main.kt
+++ src/br/com/digitalhouse/treinoDesafio/Main.kt
@@ -1,5 +1,5 @@
 package br.com.digitalhouse.treinoDesafio
 
 fun main() {
-    
+
 }
