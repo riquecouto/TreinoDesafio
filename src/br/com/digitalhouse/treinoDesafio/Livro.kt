9542935673a873987f4e9bcb881c96e7779a84fc
Victória Gamarano Prieto <viqprieto@gmail.com>
2020-09-23 16:21:49 -0300

    classe Livro

--- src/br/com/digitalhouse/treinoDesafio/Livro.kt
+++ src/br/com/digitalhouse/treinoDesafio/Livro.kt
@@ -1,4 +1,4 @@
 package br.com.digitalhouse.treinoDesafio
 
-class Livro(val titulo: String, val autor: String, val anoDeLancamento: Int) {
+class Livro(val titulo: String, val autor: String, val anoDeLancamento: Int){
 }
