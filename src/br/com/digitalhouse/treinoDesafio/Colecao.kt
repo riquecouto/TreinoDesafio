0b71a2f38aa0b6a0130ffeda5b94708500a4bfb0
Henrique <henriquecouto@poli.ufrj.br>
2020-09-23 16:41:36 -0300

    merge colecao

--- src/br/com/digitalhouse/treinoDesafio/Colecao.kt
+++ src/br/com/digitalhouse/treinoDesafio/Colecao.kt
@@ -1,4 +1,8 @@
 package br.com.digitalhouse.treinoDesafio
 
-class Colecao {
+class Colecao (
+    override var codigo: Int,
+    override var preco: Double,
+    override var quantidadeEmEstoque: Int,
+    var livro: MutableList<Livro>): Item {
 }
--- src/br/com/digitalhouse/treinoDesafio/Item.kt
+++ src/br/com/digitalhouse/treinoDesafio/Item.kt
@@ -1,4 +1,7 @@
 package br.com.digitalhouse.treinoDesafio
 
 interface Item {
+    var codigo: Int
+    var preco: Double
+    var quantidadeEmEstoque: Int
 }
